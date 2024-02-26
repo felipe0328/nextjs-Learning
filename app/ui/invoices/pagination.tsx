3fadb5f86052304ad9fcc672d155d3855835b703
Felipe Puentes <feliipepuentes@gmail.com>
2024-02-26 09:58:00 -0500

    applied prettier

--- app/ui/invoices/pagination.tsx
+++ app/ui/invoices/pagination.tsx
@@ -7,15 +7,14 @@ import { generatePagination } from '@/app/lib/utils';
 import { usePathname, useSearchParams } from 'next/navigation';
 
 export default function Pagination({ totalPages }: { totalPages: number }) {
-
   const pathname = usePathname();
   const searchParams = useSearchParams();
   const currentPage = Number(searchParams.get('page')) || 1;
 
   const createPageURL = (pageNumber: number | string) => {
     const params = new URLSearchParams(searchParams);
-    params.set('page', pageNumber.toString())
-    return `${pathname}?${params.toString()}`
+    params.set('page', pageNumber.toString());
+    return `${pathname}?${params.toString()}`;
   };
 
   const allPages = generatePagination(currentPage, totalPages);
--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental:{
-        serverActions:{
-            allowedOrigins: ['192.168.80.14:3000']
-        },
+  experimental: {
+    serverActions: {
+      allowedOrigins: ['192.168.80.14:3000'],
     },
+  },
 };
 
 module.exports = nextConfig;
